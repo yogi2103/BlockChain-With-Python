333d18bb7558094c8a24678a5bd1a736a07cae08
Kabira Singh <yogesh.baghel86@gmail.com>
2021-06-13 18:05:53 +0530

    Improving the blockchain validation logic

--- blockchain.py
+++ blockchain.py
@@ -2,11 +2,14 @@
 genesis_block={
     'previous_hash': '', 
     'index':0,
-    'transactionns':[]
+    'transactions':[]
 }
 blockchain = [genesis_block]
 open_transactions=[]
 owner='Max'
+
+def hash_block(block):
+    return '-'.join([str(block[key]) for key in block])
 
 def get_last_blockchain_value():
     """ Returns the last value of the current blockchain. """
@@ -38,12 +41,11 @@ def add_transaction(recepient,sender=owner,amount=1.0):
 
 def mine_block():
     last_block=blockchain[-1]
-    hashed_block='-'.join([str(last_block[key]) for key in last_block])
-    print(hashed_block)
+    hashed_block=hash_block(last_block)
     block={
         'previous_hash': hashed_block, 
         'index':len(blockchain),
-        'transactionns':open_transactions
+        'transactions':open_transactions
     }
     blockchain.append(block)
     pass
@@ -71,19 +73,13 @@ def print_blockchain_elements():
 
 
 def verify_chain():
-    block_index = 0
-    is_valid = True
-    for block in blockchain:
-        if block_index == 0:
-            block_index += 1
+    for (index,block) in enumerate(blockchain):
+        if index==0:
             continue
-        elif block[0] == blockchain[block_index - 1]:
-            is_valid = True
-        else:
-            is_valid = False
-            break
-        block_index += 1
-    return is_valid
+        if block['previous_hash'] != hash_block(blockchain[index-1]):
+            return False
+        return True
+
 
 waiting_for_input= True
 while waiting_for_input:
@@ -105,14 +101,23 @@ while waiting_for_input:
         print_blockchain_elements()
     elif user_choice == 'h':
         if len(blockchain) >= 1:
-            blockchain[0] = [2]
+            blockchain[0] = {
+                'previous_hash': '', 
+                'index':0,
+                'transactions':[{
+                    'sender': 'Chris',
+                    'recepient':'Max',
+                    'amount': 100.0
+                }]
+                }
     elif user_choice == 'q':
         waiting_for_input= False
     else:
         print('Input was invalid, please pick a value from the list!')
-    # if not verify_chain():
-    #     print('Invalid blockchain!')
-    #     break
+    if not verify_chain():
+        print_blockchain_elements()
+        print('Invalid blockchain!')
+        break
 
 else:
     print('User left')
