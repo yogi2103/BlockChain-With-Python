27001c6354a5be3935bec3a0ce1f5a570d251672
Kabira Singh <yogesh.baghel86@gmail.com>
2021-06-13 16:46:22 +0530

    List comprehensions

--- blockchain.py
+++ blockchain.py
@@ -38,10 +38,7 @@ def add_transaction(recepient,sender=owner,amount=1.0):
 
 def mine_block():
     last_block=blockchain[-1]
-    hashed_block=''
-    for key in last_block:
-        value=last_block[key]
-        hashed_block+=str(value)
+    hashed_block='-'.join([str(last_block[key]) for key in last_block])
     print(hashed_block)
     block={
         'previous_hash': hashed_block, 
